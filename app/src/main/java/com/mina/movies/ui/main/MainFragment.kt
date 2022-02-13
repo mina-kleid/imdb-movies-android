3133a2d5070b3626964fbd261a2690c8554687e7
Mina Kleid <minazakiz@gmail.com>
2022-02-13 16:24:26 +0100

    add tablayout and viewpager

--- app/src/main/java/com/mina/movies/ui/main/MainFragment.kt
+++ app/src/main/java/com/mina/movies/ui/main/MainFragment.kt
@@ -1,32 +1,37 @@
 package com.mina.movies.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mina.movies.R
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
+import com.mina.movies.databinding.MainFragmentBinding
+import com.mina.movies.ui.main.tabs.TabCollectionAdapter
 
 class MainFragment : Fragment() {
+    private lateinit var binding: MainFragmentBinding
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
-    private lateinit var viewModel: MainViewModel
+    private lateinit var tabCollectionAdapter: TabCollectionAdapter
+    private lateinit var viewPager: ViewPager2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        tabCollectionAdapter = TabCollectionAdapter(this)
+        viewPager = binding.pager
+        viewPager.adapter = tabCollectionAdapter
+
+        TabLayoutMediator(binding.tabLayout, viewPager) { tab, position ->
+            tab.text = TabCollectionAdapter.TABS[position].title
+        }.attach()
     }
 
 }
---
+++ app/src/main/java/com/mina/movies/ui/main/tabs/Tab.kt
@@ -0,0 +1,5 @@
+package com.mina.movies.ui.main.tabs
+
+import androidx.fragment.app.Fragment
+
+data class Tab constructor(val title: String, val fragment: Fragment)
