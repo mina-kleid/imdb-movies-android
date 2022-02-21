3e6dbef76479b346423b5598540ccea4cae7e337
Mina Kleid <minazakiz@gmail.com>
2022-02-21 21:49:14 +0100

    adding heart icon to toolbar

--- movie/src/main/java/com/mina/movie/MovieFragment.kt
+++ movie/src/main/java/com/mina/movie/MovieFragment.kt
@@ -1,9 +1,7 @@
 package com.mina.movie
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -29,10 +27,29 @@ internal class MovieFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val movie: Movie = MovieJsonConverter.fromJson(args.movie)
         (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         (requireActivity() as AppCompatActivity).supportActionBar?.title = movie.title
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_favorite -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 }
