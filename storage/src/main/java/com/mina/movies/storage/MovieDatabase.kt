6d400141b556297bcb1a2fb4ba4437f284c90df7
Mina Kleid <minazakiz@gmail.com>
2022-02-27 17:27:03 +0100

    adding missing fields to movie entity and upgrading DB version

--- storage/src/main/java/com/mina/movies/storage/MovieDatabase.kt
+++ storage/src/main/java/com/mina/movies/storage/MovieDatabase.kt
@@ -1,9 +1,16 @@
 package com.mina.movies.storage
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [MovieEntity::class], version = 1)
+@Database(
+    entities = [MovieEntity::class],
+    version = 2,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ]
+)
 internal abstract class MovieDatabase: RoomDatabase() {
     abstract fun movieDao(): MovieDao
 }
--- storage/src/main/java/com/mina/movies/storage/MovieEntity.kt
+++ storage/src/main/java/com/mina/movies/storage/MovieEntity.kt
@@ -5,5 +5,8 @@ import androidx.room.Entity
 @Entity(primaryKeys = ["title", "year"], tableName = "Movie")
 internal data class MovieEntity(
     val title: String,
-    val year: String
+    val year: String,
+    val rating: String,
+    val description: String,
+    val posterUrl: String
 )
