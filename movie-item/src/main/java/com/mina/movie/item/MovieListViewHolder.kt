1284e47840a1bad9f5472f3c5a0667b687dcd2c4
Mina Kleid <minazakiz@gmail.com>
2022-03-05 11:37:48 +0000

    working on list item layout

--- movie-item/src/main/java/com/mina/movie/item/MovieListViewHolder.kt
+++ movie-item/src/main/java/com/mina/movie/item/MovieListViewHolder.kt
@@ -1,17 +1,25 @@
 package com.mina.movie.item
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mina.common.models.Movie
 
 public class MovieListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(movie: Movie, itemClickListener: MovieListItemClickListener) {
-        val movieName: TextView = itemView.findViewById(R.id.movieTitle)
-        val movieDetails: TextView = itemView.findViewById(R.id.movieDetails)
-        movieName.text = movie.title
-        movieDetails.text = "${movie.rating} ${movie.year}"
+        val movieTitle: TextView = itemView.findViewById(R.id.movieTitle)
+        val movieDescription: TextView = itemView.findViewById(R.id.movieDescription)
+        val movieRating: TextView = itemView.findViewById(R.id.movieRating)
+        val moviePoster: ImageView = itemView.findViewById(R.id.moviePoster)
+        movieTitle.text = movie.title
+        movieDescription.text = movie.description
+        movieRating.text = movie.rating
+        movie.posterUrl?.let {
+            Glide.with(itemView.context).load(it).into(moviePoster)
+        }
         itemView.setOnClickListener { itemClickListener.onItemClicked(movie) }
     }
 }
