72f7081e2fd1239ba32de46778f92a350e3b599f
Mina Kleid <minazakiz@gmail.com>
2022-04-11 13:42:39 +0200

    attaching swipe handler to fragment

--- movie-list/src/main/java/com/mina/movie/search/MovieListModule.kt
+++ movie-list/src/main/java/com/mina/movie/search/MovieListModule.kt
@@ -2,6 +2,7 @@ package com.mina.movie.search
 
 import androidx.fragment.app.Fragment
 import com.mina.movie.item.MovieListItemClickListener
+import com.mina.movie.item.MovieListItemSwipeListener
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,4 +15,8 @@ internal object MovieListModule {
     @Provides
     fun itemClickListener(fragment: Fragment): MovieListItemClickListener =
         fragment as MovieListItemClickListener
+
+    @Provides
+    fun itemSwipeListener(fragment: Fragment): MovieListItemSwipeListener =
+        fragment as MovieListItemSwipeListener
 }
